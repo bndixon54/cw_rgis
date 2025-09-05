f444d9fee403fe5f98a8336ad21270c85949a134
bndixon54 <baileydixon54@gmail.com>
2025-09-04 21:30:39 -0400

    Fixed tidyverse02

--- code/tidyverse02.R
+++ code/tidyverse02.R
@@ -1,3 +1,25 @@
+install.packages("tidyverse")
 library(tidyverse)
 
-#this folder is empty because I was working in console the whole time to do the swirl modules 
+library(swirl)
+install_course_github("sysilviakim", "swirl-tidy")
+swirl()
+
+Bailey
+
+##The question that kept getting me into a loop if I answered it wrong: 
+##(I went into github to check what the correct answer was and it let me continue with no issue after this)
+#Check that there are indeed only five different cut values using {table} on the `cut` column of either dataset.
+
+#Answer: table(diamonds$cut)
+
+
+#Using the same principle, create from "diamonds" (without using "diamonds_grouped") a summary dataframe that produces the average price by `clarity`. Notice that we are no longer interested in average price by `cut`. Name this summarized value to be `mean_price` as before.
+
+#Answer:  diamonds %>%
+#           group_by(clarity) %>%
+#           summarize(mean_price = mean(price))
+
+#Tip: once the group-level operations/calculations have been completed, make sure to {ungroup} your dataframes. Sometimes, you may forget that your dataframe is still grouped, creating errors in your calculations.
+
+###Okay, I was able to finish this out without coming across the same issue. This "lesson" was definitely the trickiest so far. 
