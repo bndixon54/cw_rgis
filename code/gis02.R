97fb2ac3a8f19129d846535f1ad8a3365d69da38
bndixon54 <baileydixon54@gmail.com>
2025-09-25 14:09:52 -0400

    gis02

---
+++ code/gis02.R
@@ -0,0 +1,3 @@
+#vector data is point data; single combination of latitude and longitude will define where you are. 
+#if two points are connected it becomes a line vector
+#three lines connected becomes a polygon vector
