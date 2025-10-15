6292cd65c3eda6b53f6c0ed1c57971945a2fd9f3
bndixon54 <baileydixon54@gmail.com>
2025-10-15 13:31:54 -0400

    gis04

---
+++ code/gis04.R
@@ -0,0 +1,9 @@
+#October 9 2025: Raster Data I
+
+if (!require(pacman)) install.packages("pacman")
+
+pacman::p_load(tidyverse,
+               terra,
+               tidyterra,
+               mapview,
+               stars)
